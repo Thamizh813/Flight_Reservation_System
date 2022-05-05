c65638d1050959552038f3ba2a9a5806b1ea1aee
Thamizh <thamizarasan813@gmail.com>
2022-05-05 20:39:26 +0530

    i

--- src/main/java/com/flight/FlightReservationSystemApplication.java
+++ src/main/java/com/flight/FlightReservationSystemApplication.java
@@ -2,6 +2,7 @@ package com.flight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class FlightReservationSystemApplication {
+public class FlightReservationSystemApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightReservationSystemApplication.class, args);
